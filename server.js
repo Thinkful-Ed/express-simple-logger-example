f3bcb66499e296b903a4547f8e4b0b5b3481237b
Benjamin E White <benjamin.e.white1@gmail.com>
2016-10-10 11:41:48 -0400

    Updated at hyperdev.com

--- server.js
+++ server.js
@@ -24,6 +24,6 @@ app.get("/", (req, res) => res.sendFile(`${__dirname}/views/index.html` ));
 app.get("/new-url-1", (req, res) => res.send("new-url-1"));
 app.get("/new-url-2", (req, res) => res.send("new-url-2"));
 
-const listener = app.listen(process.env.PORT, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
-});
+app.listen(process.env.PORT || 8080, () => console.log(
+  `Your app is listening on port ${process.env.PORT || 8080}`));
+
